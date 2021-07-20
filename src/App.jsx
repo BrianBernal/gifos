bfa30562d8fc7557287e1cc7495e5db147339871
Brian Bernal <brianbernal27@gmail.com>
2021-07-20 14:31:41 -0500

    Cleaning the setup.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <StrictMode>
+      <div className="App">hello gifos</div>;
+    </StrictMode>
+  );
+}
+
+export default App;
